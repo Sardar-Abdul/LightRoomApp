69c60f874fae2c7e0fbde04f17086d3f41e3b14d
Sardar-Abdul <Sardar-Abdul@users.noreply.github.com>
2020-06-15 22:32:52 +0500

    Update Room.js

--- src/Room.js
+++ src/Room.js
@@ -4,13 +4,13 @@ import './Room.css';
 function Room(){
     const [ islit, notlit ] = useState (false) ;
 function on(){
-    notlit (true);
+    notlit(true);
 }
 function off(){
-    notlit ( false );
+    notlit(false);
 }
 return(
-    <div className= {'room ${islit? "lit" : "dark"}'}>
+    <div className= {`room ${islit? "lit" : "dark"}`}>
         This room is {islit ? "lit" : "dark"}
         <button onClick={on}>Click to turn on lights!</button>
         <button onClick={off}>Click to turn off lights!</button>
